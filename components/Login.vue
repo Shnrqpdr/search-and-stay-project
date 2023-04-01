ff77fcadaeaafcf458d812eaed00f933e0db1703
Shnrqpdr <hnrqpdr.sc@gmail.com>
2023-04-01 17:50:40 -0300

    login page

---
+++ components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container d-flex justify-center align-center">
+    <div class="form-div">
+        <b-form class="form-content" @submit.prevent="onSubmit">
+            <h1 class="text-center">Search and Stay Task</h1>
+            <b-form-group id="email-group" label="Email:" label-for="email">
+                <b-form-input id="email" v-model="email" type="email" required></b-form-input>
+            </b-form-group>
+            <b-form-group id="password-group" label="Password:" label-for="password">
+                <b-form-input id="password" v-model="password" type="password" required></b-form-input>
+            </b-form-group>
+            <b-button type="submit" variant="primary">Login</b-button>
+        </b-form>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+    name: 'LoginPage',
+    data() {
+      return {
+        email: '',
+        password: '',
+      };
+    },
+    methods: {
+      onSubmit() {
+        // Aqui você pode adicionar a lógica para fazer a autenticação do usuário
+        console.log(`Email: ${this.email}, Password: ${this.password}`);
+      },
+    },
+  };
+</script>
+  
+<style lang="scss" scoped>
+.form-div {
+width: 600px;
+margin: auto;
+padding: 20px;
+background-color: white;
+}
+
+.form-content {
+    width: 100%;
+}
+</style>
+
+  
+  
+  
+  
+  
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial />
+  <Login />
 </template>
 
 <script>
+import Login from '../components/Login.vue';
+
 export default {
-  name: 'IndexPage',
+    name: "IndexPage",
+    components: { Login }
 }
 </script>
+<style lang="scss">
+#__layout {
+  background: aliceblue;
+}
+
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
